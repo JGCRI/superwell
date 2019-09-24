af2bedf7a3a7247b3a62f72746891439e5cec078
nkholod <nazar.kholod@pnnl.gov>
2019-09-24 13:42:47 -0400

    Added a new output srtucture. The runtime is about 40 minutes

--- superwell.R
+++ superwell.R
@@ -52,7 +52,7 @@ ec <- yaml.load_file(el_cost_file)
 # @param Depletion_Limit <- 0.05 
 # @param Energy_cost_rate <- 0.074
 # @param Interest_Rate <- 0.1
-# @paramMaintenance_factor <- 0.07
+# @param Maintenance_factor <- 0.07
 # @param Max_lifetime_in_Years <- 20
 # @param Pump_efficiency <- 0.5
 
@@ -115,12 +115,7 @@ file_name <- paste(df[1, "CNTRY_NAME"], "_WellResults.csv")
 con <- file(file_name, "w")
 
 # Write the headers for the output file
-# The original header was cat("Continent,ObjID,Country,t,Drawdown,ObservedDrawdown,Volume,ElementArea,ArealExtent,TotalHead,Power,
-# ElectricEnergy,EnergyCostRate,CostOfEnergy,UnitCost,CostPerAcFt,Interest_Rate,Max_Lifetime_in_Years,Maintenance_factor,
-# Well_Yield,Annual_Operation_time,Total_Well_Length,WHYClass,Available_Volume,Number of Wells, Total Time,BasinId,BasinName,\n",file=con)
-
-# The new format is from folder "R code/MENA_Results_161216/updatedformat/5/superwell.R
-cat("Iteration,t,Unit_Cost,Hydraulic_Conductivity,Radial_Extent,Wells,Volume_Produced,Total_Volume_Produced,Total_Volume_Available,Continent,ObjId,Country,GCAMid,Basin_Name,\n", file = con)
+cat("Continent,ObjID,Country,time,Drawdown,Observed_Drawdown,Volume,Element_Area,Areal_Extent,Total_Head,Power,Electric_Energy,Energy_Cost_Rate,Cost_of_Energy,Unit_Cost,Cost_Per_Ac_Ft,Interest_Rate,Max_Lifetime_in_Years,Maintenance_factor,Well_Yield,Annual_Operation_time,Total_Well_Length,WHYClass,Available_Volume,Number_of_Wells, Total_Time,Basin_ID,Basin_Name\n", file = con)
 
 # -----
 # Calculations 
@@ -139,13 +134,13 @@ for(i in 1:(nrow(df))) {
 	wp[["Depth_to_Piezometric_Surface"]] <- df[i,"Depth"]
 	wp[["Aqfr_Sat_Thickness"]] <- df[i,"Thickness"]                                                                                     # m
 	wp[["Orig_Aqfr_Sat_Thickness"]] <- df[i,"Thickness"]                                                                                # m
-	wp[["Screen_length"]] <- wp$Orig_Aqfr_Sat_Thickness * 0.3                                                                             # m
-	wp[["Casing_length"]] <- wp$Orig_Aqfr_Sat_Thickness + wp$Depth_to_Piezometric_Surface - wp$Screen_length                              # m
+	wp[["Screen_length"]] <- wp$Orig_Aqfr_Sat_Thickness * 0.3                                                                           # m
+	wp[["Casing_length"]] <- wp$Orig_Aqfr_Sat_Thickness + wp$Depth_to_Piezometric_Surface - wp$Screen_length                            # m
 #	wp[["Max_Drawdown"]] <- 0.66 * wp$Aqfr_Sat_Thickness                                                                                # m
 	wp[["Total_Well_Length"]] <- wp$Casing_length + wp$Screen_length                                                                    # m
 	wp[["Hydraulic_Conductivity"]] <- (10 ^ df[i,"Permeability"]) * 1e7
 	wp[["roi_boundary"]] <- 1
-	wp[["Transmissivity"]] <- wp$Hydraulic_Conductivity * wp$Screen_length     ###this changed. Is it right?                            # m2/s
+	wp[["Transmissivity"]] <- wp$Hydraulic_Conductivity * wp$Screen_length                                                              # m2/s
 	if (df[i,"WHYClass"] == 10) {
 		wp[["Well_Installation_cost"]] <- wp$Well_Install_10 * wp$Total_Well_Length
 	} else {
